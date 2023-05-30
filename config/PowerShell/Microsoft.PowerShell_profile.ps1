9fbd29fe17df91b4d042837adf31cdfdb83a21e0
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-31 00:45:57 +0900

    windows terminal: change theme nord -> dracula

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\agnoster.minimal.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\dracula.omp.json" | Invoke-Expression
 
 # PSReadLine
 Import-Module PSReadLine
