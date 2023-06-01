c817f58624dddf2402f45e2f4032f49a7435f173
SuperYuro <a.gpgtdmgp@gmail.com>
2023-06-01 16:49:20 +0900

    Update configure.ps1 to put config file for clang-format

--- script/configure.ps1
+++ script/configure.ps1
@@ -1,5 +1,6 @@
-New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\PowerToys" -Path "$env:USERPROFILE\Documents" -Name "PowerToys" -ItemType SymbolicLink
-New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\PowerShell" -Path "$env:USERPROFILE\Documents" -Name "PowerShell" -ItemType SymbolicLink
+sudo New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\PowerToys" -Path "$env:USERPROFILE\Documents" -Name "PowerToys" -ItemType SymbolicLink
+sudo New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\PowerShell" -Path "$env:USERPROFILE\Documents" -Name "PowerShell" -ItemType SymbolicLink
 
-New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\.gitconfig" -Path "$env:USERPROFILE" -Name ".gitconfig" -ItemType SymbolicLink
-New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\.wslconfig" -Path "$env:USERPROFILE" -Name ".wslconfig" -ItemType SymbolicLink
+sudo New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\.gitconfig" -Path "$env:USERPROFILE" -Name ".gitconfig" -ItemType SymbolicLink
+sudo New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\.wslconfig" -Path "$env:USERPROFILE" -Name ".wslconfig" -ItemType SymbolicLink
+sudo New-Item -Value "$env:USERPROFILE\dotfiles-windows\config\.clang-format" -Path "$env:USERPROFILE" -Name ".clang-format" -ItemType SymbolicLink
