343dad8a0bd92c56d2a828f5d2491729b86d537e
SuperYuro <a.gpgtdmgp@gmail.com>
2023-07-11 20:18:15 +0900

    Restore colorscheme
    This reverts commit 9ec40048311f703fc291a0303674b79d69d67431.

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\dracula.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
 
 # PSReadLine
 Import-Module PSReadLine
