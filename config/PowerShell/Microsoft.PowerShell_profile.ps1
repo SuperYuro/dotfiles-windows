dce3012a0a6726d292eb7d7770859a85bc83405c
SuperYuro <a.gpgtdmgp@gmail.com>
2023-12-21 23:45:26 +0900

    feat: PowerShellをシンプルにした

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,47 +1,46 @@
 # Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hotstick.minimal.omp.json" | Invoke-Expression
 
 # PSReadLine
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
+# Set-PSReadLineOption -EditMode Emacs
+# Set-PSReadLineOption -BellStyle None
 # Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Alias
-$ConfigDir="$HOME\Documents\PowerShell"
-$LsdConfig="$ConfigDir\lsd.ps1"
+# $ConfigDir="$HOME\Documents\PowerShell"
+# $LsdConfig="$ConfigDir\lsd.ps1"
 
-. $LsdConfig
+# . $LsdConfig
 
-Set-Alias c cd
-Set-Alias v nvim
-Set-Alias g git
-Set-Alias th New-Item
+# Set-Alias c cd
+# Set-Alias v nvim
+# Set-Alias g git
+# Set-Alias th New-Item
 
-function GotoGhqRepository
-{
-    $repo = ghq list | peco
-    $root = ghq root
+# function GotoGhqRepository
+# {
+#     $repo = ghq list | peco
+#     $root = ghq root
+#
+#     if($repo)
+#     {
+#         Set-Location (Join-Path $root $repo)
+#     } else
+#     {
+#         Set-Location ".\"
+#     }
+# }
 
-    if($repo)
-    {
-        Set-Location (Join-Path $root $repo)
-    } else
-    {
-        Set-Location ".\"
-    }
-}
+# Set-Alias ghl GotoGhqRepository
+# Set-Alias lg lazygit
 
-Set-Alias ghl GotoGhqRepository
-Set-Alias lg lazygit
+# function DuplicateTab
+# {
+#     $currentPath = Get-Location
+#     wt -w 0 nt -d $currentPath
+# }
 
-function DuplicateTab
-{
-    $currentPath = Get-Location
-    wt -w 0 nt -d $currentPath
-}
-
-Set-Alias tm DuplicateTab
-
+# Set-Alias tm DuplicateTab
